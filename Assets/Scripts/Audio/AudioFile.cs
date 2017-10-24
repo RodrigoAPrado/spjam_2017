08952df954157f4b0055f52f9245d5d28e0c6513
Rodrigo <rodrigo.almprado@gmail.com>
2017-10-23 22:51:45 -0200

    Criado sistema de audio manager.

---
+++ Assets/Scripts/Audio/AudioFile.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioFile {
+
+    protected AudioClip audio;
+    protected float baseVolume;
+    protected float baseSpeed;
+
+    public AudioFile()
+    {
+
+    }
+
+    public AudioFile(string audioPath, float baseVolume = 1, float baseSpeed = 1)
+    {
+        this.audio = Resources.Load(audioPath) as AudioClip;
+        this.baseVolume = baseVolume;
+        this.baseSpeed = baseSpeed;
+    }
+
+    public float getVolume()
+    {
+        return baseVolume;
+    }
+
+    public float getSpeed()
+    {
+        return baseSpeed;
+    }
+
+    public AudioClip getAudio()
+    {
+        return audio;
+    }
+}
---
+++ Assets/Scripts/Audio/MusicPlayer.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+    
+    private MusicFile defaultMusic;
+    
+    private MusicFile currentMusic;
+
+    private MusicController musicController;
+
+    private AudioSource audioSource;
+
+    // Use this for initialization
+    void Start () {
+        Object.DontDestroyOnLoad(this.gameObject);
+
+        musicController = new MusicController();
+
+        defaultMusic = new MusicFile("MINHAROLA\\Hip.mp3");
+
+        currentMusic = defaultMusic;
+
+        audioSource = gameObject.GetComponent<AudioSource>();
+
+        if(audioSource != null)
+        {
+            startMusic();
+        }else
+        {
+            throw new System.Exception("Game object does not have an Audio Source. Cannot play music");
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(currentMusic.getLoopEnd() != 0)
+        {
+            if(audioSource.time >= currentMusic.getLoopEnd())
+            {
+                audioSource.time = currentMusic.getLoopStart();
+            }
+        }
+        if (!audioSource.isPlaying)
+        {
+            audioSource.Play();
+        }
+    }
+
+    void startMusic()
+    {
+        if(currentMusic == null)
+        {
+            throw new System.Exception("Current music is invalid or non-existent. Cannot play music");
+        }
+        if (audioSource.isPlaying)
+        {
+            audioSource.Stop();
+        }
+        audioSource.clip = currentMusic.getAudio();
+        musicSettings();
+        audioSource.Play();
+        
+    }
+
+    void musicSettings()
+    {
+        audioSource.volume = musicController.getVolume() * currentMusic.getVolume();
+        audioSource.mute = musicController.getMute();
+    }
+    
+}
